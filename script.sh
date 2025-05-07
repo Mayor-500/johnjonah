39fa64de21f633c87c9d21e57d695f0859f1d2ff
John Jonah (Mayor-500) <johnjonah895@gmail.com>
2025-05-07 14:57:07 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,6 @@ mkdir chidifile
 cd chidifile
 touch chidi.txt
 echo "change your laptop to windows sophia"  >> chidifile.txt
+echo "tobita did me dirt" >> chidifile.txt
+echo "they told me class wil last for five months whereas its three months" >> chidifile.txt
 
